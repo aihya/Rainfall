3d0e499a3e778860f618e8c0e6993799f1360d48
Abdechahid Ihya <aihya@e1r6p12.1337.ma>
2022-02-19 19:04:55 +0100

    8-9

--- level8/main.c
+++ level8/main.c
@@ -4,6 +4,7 @@ char	*auth = NULL; // 0x804a008
 int		main()
 {
 	char	buffer[128];
+
 	while (1)
 	{
 		printf("%p, %p \n", auth, service);
@@ -20,7 +21,7 @@ int		main()
 			service = strdup(buffer + 7);
 		else if (strncmp(buffer, "login", 5) == 0)
 		{
-			if (auth)
+			if (auth + 0x20) // probably an int
 				system("/bin/sh");
 			else
 				fwrite("Password:\n", 0x1, 0xa, stdout);
---
+++ level9/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class N
+{
+public:
+	N(int num) {this->raqm = num};
+	void setAnnotation(int, int) {};
+	N& operator+(N& instance);
+	N& operator-(N& instance);
+private:
+	void	*something;
+	char	buffer[100];
+	int 	raqm;
+}
+
+void N::setAnnotation(char *str)
+{
+	size_t	size;
+	
+	size = strlen(str);
+	memcpy(this->buf, str, size);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 0x1)
+		exit(0x1);
+
+	N *o = new N(0x5);
+	N *p = new N(0x6);
+
+	o->setAnnotation(argv[1]);
+
+	p += o;
+}
