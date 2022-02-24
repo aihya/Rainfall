31f6c71238e478ed14b61ac023204fd0a2976140
Abdechahid Ihya <aihya@e1r6p12.1337.ma>
2022-02-24 12:28:24 +0100

    level0/main, level0/walkthrough, b2, b3

---
+++ bonus3/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	char	key[132];	// ???	esp + 0x18
+	int		fd;			// 		esp + 0x9c
+
+
+	fd = fopen("/home/user/end/.pass", "r");
+
+	memset(key, 0x0, 0x21);
+	if (fd == 0 || argc != 0x2)
+		return (-1);
+
+	fread(key, 0x1, 0x42, fd);
+	key[0x59] = '\0';
+	key[atoi(argv[1])] = '\0';
+
+	fread(key + 0x42, 0x1, 0x41, fd);
+
+	fclose(fd);
+
+	if (strcmp(key, argv[1]))
+		execl("/bin/sh", "sh", NULL);
+	else
+		puts(key + 0x42);
+	return (0);
+}
---
+++ level0/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	gid_t	egid;		// esp + 0x1c
+	gid_t	euid;		// esp + 0x18
+	char	*buffer;	// esp + 0x10
+
+	if (atoi(argv[1]) == 0x1a7) // 423
+	{
+		buffer = strdup("/bin/sh");
+		egid = getegid();
+		euid = geteuid(gid);
+		setresgid(egid, egid, egid);
+		setresuid(euid, euid, euid);
+		execv("/bin/sh", buffer);
+	}
+	else
+		fwrite("No !\n", 0x1, 0x5, stderr);
+	return (0);
+}
