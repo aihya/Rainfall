eb39860027f4c4e3b4e6655f4fd8ad3a143d371c
Abdechahid Ihya <aihya@e1r6p12.1337.ma>
2022-02-26 18:24:15 +0100

    -

--- bonus0/main.c
+++ bonus0/main.c
@@ -1,35 +1,38 @@
+#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
 
-void	p(char *buffer, char *header)
+void p(char *buff, char *header)
 {
-	char	puffer[0x1008];
+    char pBuffer[0x1000];
 
-	puts(header);
-	read(0x0, puffer, 0x1000 /* 4096 bytes */);
-	puffer[strchr(puffer, '\n')] = '\0';
-	strncpy(buffer, puffer, 0x14);
+    puts(header);
+    read(0, pBuffer, 0x1000);
+    *(strchr(pBuffer, 0xa)) = '\0';
+    strncpy(buff, pBuffer, 0x14);
 }
 
-void	pp(char *buffer)
+void pp(char *buff)
 {
-	char	buffer1[20];
-	char	buffer2[20];
+    char firstBuffer[20];
+    char secondBuffer[20];
 
-	p(buffer2, " - ");
-	p(buffer1, " - ");
+    p(firstBuffer, " - ");
+    p(secondBuffer, " - ");
 
-	strcpy(buffer, buffer2);
+    strcpy(buff, firstBuffer);
 
-	*(buffer + strlen(buffer)) = " ";
 
-	strcat(buffer, buffer1);
+    strcpy(buff + strlen(buff), " ");
+    strcat(buff, secondBuffer);
 }
 
-int	main()
-{
-	char	buffer[42];
 
-	pp(buffer);
-	puts(buffer);
-	return (0);
+int main(int argc, char **argv)
+{
+    char buff[42];
+
+    pp(buff);
+    puts(buff);
+    return 0x0;
 }
